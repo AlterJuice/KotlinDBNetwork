1468b3c3218baa73a13ed39da46a7493b89c4137
bogdantrainee <bogdan171000@gmail.com>
2021-06-17 17:44:29 +0300

    Welp no matter changes in fragment

--- app/src/main/java/com/edu/kotlindbnetwork/ui/UserListFragment.kt
+++ app/src/main/java/com/edu/kotlindbnetwork/ui/UserListFragment.kt
@@ -10,16 +10,10 @@ import com.edu.kotlindbnetwork.R
 import com.edu.kotlindbnetwork.repo.UserRepoNetwork
 import com.edu.kotlindbnetwork.viewmodels.UserListViewModel
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [UserListFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
+
+
+
 class UserListFragment : Fragment() {
 
     lateinit var model: UserListViewModel
